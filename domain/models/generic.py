a25d3fdd681c52250c6379794d7d19053bd75b63
douglaschalegre <douglas.chalegre@gmail.com>
2024-04-03 22:31:07 -0300

    chore: Update metadata schema and model configuration

--- domain/models/generic.py
+++ domain/models/generic.py
@@ -3,7 +3,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from config.database import db
 
-metadata_obj = MetaData(schema=db["schema"])
+metadata_obj = MetaData(schema=db["name"])
 Base = declarative_base(metadata=metadata_obj)
 
 ALL_DELETE = 'all, delete'
--- domain/schemas/generic.py
+++ domain/schemas/generic.py
@@ -1,19 +1,20 @@
 """Generic classes and resources for schema building."""
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from utils.camel import to_camel
 
 
 class GenericSchema(BaseModel):
     """Generic Class for schemas."""
-    class Config:
-        """Schema config class"""
-        alias_generator = to_camel
-        allow_population_by_field_name = True
+    model_config = ConfigDict(
+        alias_generator=to_camel,  # type: ignore
+        populate_by_name=True
+    )
 
 
 class TableSchema(GenericSchema):
     """Class for schemas based on database table.."""
-    class Config:
-        """Schema config class"""
-        orm_mode = True
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )

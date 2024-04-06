1624160d8eac82a4e0dc99b84b4a9c3acd831fb4
douglaschalegre <douglas.chalegre@gmail.com>
2024-04-06 16:28:27 -0300

    chore: Add new schema files for user, list, and list_user

--- domain/schemas/__init__.py
+++ domain/schemas/__init__.py
@@ -2,6 +2,9 @@
 from .generic import *
 from .app.product import *
 from .app.section import *
+from .app.user import *
+from .app.list import *
+from .app.list_user import *
 
 # Always last
 from .orm import *

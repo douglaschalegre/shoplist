8f364dd64b5d5bd2f084ce24d17e2e185124479e
douglaschalegre <douglas.chalegre@gmail.com>
2024-04-09 16:17:40 -0300

    chore: add new routes to api

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,14 +1,26 @@
 """Creation of controller list"""
 
-from . import health
-from . import product
+from . import (
+    health,
+    user,
+    list as shopping_list,
+    section,
+    product,
+)
 
 routes = [
+    user.router,
+    shopping_list.router,
+    section.router,
     product.router,
 
     health.router,  # health route must be the last one!
 ]
 tags = [
+    *user.tags,
+    *shopping_list.tags,
+    *section.tags,
     *product.tags,
+
     *health.tags,  # health tag must be the last one!
 ]

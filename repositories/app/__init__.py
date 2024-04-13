ae10d7a1b4a6e16fec7df3390aadd7a8a2f0d90a
douglaschalegre <douglas.chalegre@gmail.com>
2024-04-13 00:55:29 -0300

    chore: exporting modules

--- repositories/app/__init__.py
+++ repositories/app/__init__.py
@@ -1,3 +1,7 @@
 '''Exporting repositories of the app module'''
 from .product import *
 from .section import *
+from .list_product import *
+from .user import *
+from .list import *
+from .list_user import *

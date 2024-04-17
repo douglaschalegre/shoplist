1a0dd5f60b339c8325ffd7235dbb886e5f6aae66
douglaschalegre <douglas.chalegre@gmail.com>
2024-04-17 00:32:34 -0300

    fix: cant have type annotations on this because of circular import :(

--- utils/db_object.py
+++ utils/db_object.py
@@ -1,9 +1,7 @@
 '''Module for database object utilities'''
 
-from domain import schemas, models
 
-
-def replace_values(model: models.GenericBase, schema: schemas.GenericSchema) -> models.GenericBase:
+def replace_values(model, schema ): 
     '''Replace values of a model with values from a schema, util for updating database with edit schemas'''
     model_copy = model
     for key, value in schema.model_dump().items():

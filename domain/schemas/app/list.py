abe4a67aeebdb1ea5e85d260d3b8ec643f8f1d3a
douglaschalegre <douglas.chalegre@gmail.com>
2024-04-16 22:18:54 -0300

    feature: Add ListUserBase and ListProductBase to List schema

--- domain/schemas/app/list.py
+++ domain/schemas/app/list.py
@@ -1,8 +1,12 @@
 '''List schema'''
 from domain.schemas.orm import (
-    ListBase
+    ListBase,
+    ListProductBase,
+    ListUserBase
 )
 
 
 class List(ListBase):
     '''List schema'''
+    users: list[ListUserBase]
+    products: list[ListProductBase]
